dcf37a6f36095f9e9d5000b7621f402e54e78c44
Anu Nikkanen <anu.nikkanen@gmail.com>
2013-12-22 22:56:06 +0200

    lisatty maarittely

--- Labyrintti/src/labyrintti/algot/Astar.java
+++ Labyrintti/src/labyrintti/algot/Astar.java
@@ -3,7 +3,6 @@ package labyrintti.algot;
 
 import java.util.ArrayList;
 import java.util.PriorityQueue;
-import java.util.Set;
 import labyrintti.Solmu;
 
 /**
@@ -11,24 +10,7 @@ import labyrintti.Solmu;
  * @author Anu N.
  */
 public class Astar {
-    
-    /*Astar(G,w,a,b)
-     * G on tutkittava verkko, a lähtösolmu, b kohdesolmu ja w kaaripainot kertova funktio
-     * for kaikille solmuille v e V
-     *  alkuun[v] = ääretön
-     *  loppuun[v] = arvioi suora etäisyys v --> b
-     *  polku[v] = NIL
-     * alkuun[a] = 0
-     * S = tyhjäjoukko
-     * while (solmu b ei ole vielä joukossa S)
-     *  valitse solmu u e V \ S jolle alkuun[v] + loppuun[v] on pienin
-     *  S = S u {u}
-     *  for (jokaiselle solmulle v e Adj[u] // kaikille u:n vierussolmuille v
-     *     if alkuun[v] > alkuun [u] + w(u,v)
-     *        alkuun[v] = alkuun[u]+w(u,v)
-     *        polku[v] = u
-     */
-    
+        
     private Solmu[][] matriisi;
     private Solmu start;
     private Solmu end;
@@ -54,6 +36,11 @@ public class Astar {
         
         while(!this.avoimet.isEmpty()) {
             Solmu v = (Solmu) this.avoimet.poll();
+            if (v.equals(end)) {
+                break;
+            }
+            this.avoimet.remove(v);
+            this.kaydyt.add(v);
             
         }
     }
